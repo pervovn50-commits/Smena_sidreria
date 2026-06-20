90a1f62e110fb5e15d436dc297cbed33283249a8
pervovn50-commits <pervovn50@gmail.com>
2026-06-20 18:01:29 +0300

    Add files via upload

---
+++ config_1.py
@@ -0,0 +1,7 @@
+```python
+import os
+BOT_TOKEN           = os.environ.get("BOT_TOKEN", "YOUR_TOKEN")
+SUPERADMIN_ID       = int(os.environ.get("SUPERADMIN_ID", "0"))
+NOTIFICATIONS_CHAT_ID = int(os.environ.get("NOTIFICATIONS_CHAT_ID", "0"))
+REPORTS_CHAT_ID     = int(os.environ.get("REPORTS_CHAT_ID", "0"))
+TIMEZONE            = os.environ.get("TIMEZONE", "Europe/Moscow")
